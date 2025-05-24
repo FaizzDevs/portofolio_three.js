0a88887b4bec1fffbe4d16463ad1098ba8446d0d
FaizzDevs <faizdevs@gmail.com>
2025-05-24 15:52:00 +0700

    update code

--- src/components/Glowcards.jsx
+++ src/components/Glowcards.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import { useRef } from 'react'
 
-const Glowcards = ({ card, children }) => {
+const Glowcards = ({ card, children, index }) => {
+  const cardRefs = useRef([]); // membuat array baru untuk menyimpan card
+
+  // fungsi untuk gerakan mouse
+  const handleMouseMove = (index) => (e) => {
+    const card = cardRefs.current[index];
+    if (!card) return; // jika card tidak ada maka hentikan
+
+    const rect = card.getBoundingClientRect(); // ambil posisi ukuran pada layar
+    const mouseX = e.clientX - rect.left - rect.width / 2; // mencari posisi mouse
+    const mouseY = e.clientY - rect.top - rect.height / 2;
+
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI); // hitung arah kursor dalam derajat
+
+    angle = (angle + 360) % 360; // ubah negatif menjadi 0 - 360 derajat
+
+    card.style.setProperty("--start", angle + 60);
+  }
+
   return (
-    <div className="card relative z-40 overflowHidden transition-colors duration-1000 ease-in-out card-border border border-black-50 bg-black-100 timeline-card rounded-xl p-10">
+    <div 
+        ref={(el) => (cardRefs.current[index] = el)}
+        onMouseMove={handleMouseMove(index)}
+        className="card relative z-40 overflowHidden transition-colors duration-1000 ease-in-out card-border border border-black-50 bg-black-100 timeline-card rounded-xl p-10">
       <div className="glow pointer-none: absolute w-full h-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform blur-lg saturate-200" />
-        <div>
+        <div className="flex items-center gap-1 mb-5">
             {Array.from({ length: 5 }, (_, i) => (
-                <img src="/images/star.png" key={i} alt="Star" />
+                <img src="/images/star.png" key={i} alt="Star" className="size-5" />
             ))}
             
       </div>
+
+      <div className="mb-5">
+          <p className="text-white-50 text-lg">{card.review}</p>
+      </div>
+      {/* mendapatkan dari file experience.jsx Glowcard */}
+      {children}
     </div>
   )
 }
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -5,7 +5,7 @@ import Glowcards from '../components/Glowcards'
 
 const Experience = () => {
   return (
-    <section id="experience" className="w-full mt-20 section-padding md:mt-40 xl:px-0">
+    <section id="experience" className="w-full mt-20 section-padding px-5 md:px-10 md:mt-40 xl:px-0">
       <div className="w-full h-full px-5 md:px-20">
             <Titleheader 
                 title="Work Professional" 
@@ -13,14 +13,29 @@ const Experience = () => {
 
         <div className="mt-32 relative">
           <div className="relative z-50 space-y-10 xl:space-y-32">
-              {expCards.map((card) => (
+              {expCards.map((card, index) => (
                 <div key={card.title} className="exp-card-wrapper flex flex-col-reverse gap-10 justify-between xl:flex-row xl:gap-20 ">
                     <div className="xl:w-2/6">
-                        <Glowcards card={card}>
+                        <Glowcards card={card} index={index}>
                           <div>
                               <img src={card.imgPath} alt={card.title} />
                           </div>
                         </Glowcards>
+                    </div>
+
+                    <div className="xl:w-4/6">
+                        <div className="flex items-start">
+                            <div className="timeline-wrapper">
+                                <div className="timeline" />
+                                <div className="gradient-line w-1 h-full" />
+                            </div>
+
+                            <div className="expText flex gap-5 relative z-20 md:gap-10 xl:gap-20">
+                                <div>
+                                  
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
               ))}
